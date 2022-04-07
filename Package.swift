a1e4320040fcee9d7a1de3b56f8e087c62824c5f
杨俊艺 <1209032479@qq.com>
2022-04-07 21:32:17 +0800

    MQTTClient第三方经由Pod更新获取依赖后抽出代码整理成Swift Package

--- Package.swift
+++ Package.swift
@@ -6,21 +6,17 @@ import PackageDescription
 let package = Package(
     name: "MQTTClient",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MQTTClient",
             targets: ["MQTTClient"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MQTTClient",
-            dependencies: []),
+        .target(name: "MQTTClient", dependencies: [], path: "Sources/MQTTClient",
+                exclude: [],
+                resources: [],
+                publicHeadersPath: "",
+                cSettings: []),
         .testTarget(
             name: "MQTTClientTests",
             dependencies: ["MQTTClient"]),
